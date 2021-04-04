7fad0118626c1ee2f1b280fdfbd650f4a6654e24
plenzke <3njoyth3sil3nc399@gmail.com>
2021-04-04 23:30:46 +0900

    added homework 1

---
+++ app.py
@@ -0,0 +1,25 @@
+import time
+from flask import render_template
+import redis
+from flask import Flask
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    count = get_hit_count()
+    #return 'Количество посищений = {}.\n'.format(count)
+    return render_template("index.html", count = count)
